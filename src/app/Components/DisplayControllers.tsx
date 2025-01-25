ff4576127756b14382a8c1683e566ab712046266
Mustapha-1610 <talbimustapha.work@outlook.com>
2025-01-25 04:04:15 +0100

    improved display controllers

--- src/app/Components/DisplayControllers.tsx
+++ src/app/Components/DisplayControllers.tsx
@@ -2,17 +2,21 @@ import { FaBook, FaRegKeyboard } from "react-icons/fa6";
 
 export default function DisplayControllers() {
   return (
-    <>
-      <div className="flex gap-6 self-center max-w-full text-base text-center whitespace-nowrap w-[419px]">
-        <div className="flex flex-1 gap-3 px-6 py-4 text-black dark:text-white bg-gray-100 dark:bg-[#1f2937] rounded-xl max-md:px-5 items-center justify-center cursor-pointer">
-          <FaBook />
+    <div className="flex justify-center my-4">
+      <div className="flex gap-6 self-center text-base text-center">
+        <div className="flex items-center gap-3 px-4 py-3 text-black dark:text-white bg-gray-100 dark:bg-[#1f2937] rounded-xl cursor-pointer w-48 h-16 justify-center">
+          <FaBook size={24} />
           <div>Comprehension</div>
         </div>
-        <div className="flex flex-1 gap-3 px-6 py-4 text-gray-800 dark:text-gray-100 bg-gray-200 dark:bg-[#374151] rounded-xl max-md:px-5 items-center justify-center cursor-pointer">
-          <FaRegKeyboard size={20} className="mb-0.5" />
+        <div className="flex items-center gap-3 px-4 py-3 text-gray-800 dark:text-gray-100 bg-gray-200 dark:bg-[#374151] rounded-xl cursor-pointer w-48 h-16 justify-center">
+          <FaRegKeyboard size={24} />
           <div>Writing</div>
         </div>
+        <div className="flex items-center gap-3 px-4 py-3 text-black dark:text-white bg-gray-100 dark:bg-[#1f2937] rounded-xl cursor-pointer w-48 h-16 justify-center">
+          <FaBook size={24} />
+          <div>Grammar</div>
+        </div>
       </div>
-    </>
+    </div>
   );
 }
